bde1acc57b32247ab09d07dc93e44f4e0ba4e016
kamuizero <aureliopinero@gmail.com>
2019-07-21 10:18:47 +0900

    Add template file, handle request from server with value

--- src/main/java/com/aepm/springboottest/controllers/MainController.java
+++ src/main/java/com/aepm/springboottest/controllers/MainController.java
@@ -2,21 +2,25 @@ package com.aepm.springboottest.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@RequestMapping("/")
+@Controller
+//@RequestMapping("/") //Aqui definimos un prefijo para las requests que va a manejar este controlador
 public class MainController {
 
     Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    @GetMapping("/")
-    public String index() {
-        logger.info("Entro en el GET del main");
-        return "Prueba!!!!!";
+    @GetMapping("/saludo")
+    public String index(@RequestParam (name="name", required=false, defaultValue="World") String name, Model model) {
+        logger.debug("Valor de 'name' enviado al servidor es: " + name);
+        model.addAttribute("valor", name);
+        return "value";
     }
 
 }
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+console.log('Load')
+
+function apiCallHandler (a, b) {
+    console.log('Entro en el API CALL')
+    console.log('Resultado de la suma de los numeros es: ', a + b)
+}
